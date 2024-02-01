63b82dad1467c3cb99ab2b5e2a43807d3d481c69
bharatjena123 <bharatjena2021@gmail.com>
2024-02-01 18:22:11 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.models import *
+from django.db.models import Q
 
 
 
@@ -37,9 +38,37 @@ def selfjoins(request):
     EMO=Emp.objects.select_related('mgr').filter(deptno__dname='SALES')
     EMO=Emp.objects.select_related('mgr').filter(hiredate_year=2024)
 
-    
-
 
 
     d={'EMO':EMO}
     return render(request,'selfjoins.html',d)
+
+
+
+
+
+def emp_mgr_dept(request):
+    EMD=Emp.objects.select_related('deptno','mgr').all()
+    EMD=Emp.objects.select_related('deptno','mgr').filter(ename='SMITH')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(sal__gte=2500)
+    EMD=Emp.objects.select_related('deptno','mgr').filter(mgr__ename='KING')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(mgr__deptno='20')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(deptno__dname='SALES')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(deptno__dloc='NEW YORK')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(mgr__ename='BLAKE')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(sal__lte=2000)
+    EMD=Emp.objects.select_related('deptno','mgr').filter(deptno__dloc='NEW YORK')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(ename='SMITH')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(mgr__deptno=30)
+    EMD=Emp.objects.select_related('deptno','mgr').filter(deptno__dloc='DALLAS')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(deptno__dname='SALES')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(ename__startswith='A')
+    EMD=Emp.objects.select_related('deptno','mgr').filter(ename='SMITH',sal__lte=1000)
+    EMD=Emp.objects.select_related('deptno','mgr').filter(Q(hiredate__year=2024) |Q(ename__startswith='A'))
+    EMD=Emp.objects.select_related('deptno','mgr').filter(deptno__dname='RESEARCH')
+    
+
+
+                                
+    d={'EMD':EMD}
+    return render(request,'emp_mgr_dept.html',d)
--- project38/urls.py
+++ project38/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('equijoins/',equijoins,name='equijoins'),
     path('selfjoins/',selfjoins,name='selfjoins'),
+    path('emp_mgr_dept/',emp_mgr_dept,name='emp_mgr_dept'),
 ]
